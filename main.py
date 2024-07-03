4485f7c5b5324d583855e78809ff41e77d2b2439
Jivelien <julien.delannoy@hotmail.com>
2024-07-03 16:53:04 +0200

    correct requirements

--- main.py
+++ main.py
@@ -11,18 +11,18 @@ def visualisation_biscuit():
     cv2.startWindowThread()
 
     cv2.namedWindow("Anthology", cv2.WND_PROP_ASPECT_RATIO)
-    cv2.setWindowProperty("Anthology", cv2.WND_PROP_AUTOSIZE, cv2.WINDOW_GUI_EXPANDED)
+    cv2.setWindowProperty("Anthology", cv2.WND_PROP_ASPECT_RATIO, cv2.WINDOW_GUI_EXPANDED)
 
-    ants = [Ant(position=Position(x=0, y=0)) for _ in range(50)]
+    ants = [Ant(position=Position(x=0, y=0)) for _ in range(100)]
 
-    canva_x = 1000
-    canva_y = 1000
+    canva_x = 2000
+    canva_y = 2000
     while True:
         world = np.array(np.zeros((canva_x, canva_y)))
         canva = cv2.cvtColor(world.astype('uint8'), cv2.COLOR_GRAY2RGB)
 
         for ant in ants:
-            ant.rotate(angle=randint(-30, 30))
+            ant.rotate(angle=randint(-5, 5))
             ant.move()
             x = int(ant.position.x + canva_x/2)%canva_x
             y = int(ant.position.y + canva_y/2)%canva_y
